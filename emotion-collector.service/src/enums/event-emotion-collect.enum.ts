030e279a1d511b62cfe50511782f290c6bf85b00
chabuuuu <haphuthinh332004@gmail.com>
2025-06-08 11:50:21 +0700

    feat(redis): add ioredis integration and update enums for emotion collection

--- emotion-collector.service/src/enums/event-emotion-collect.enum.ts
+++ emotion-collector.service/src/enums/event-emotion-collect.enum.ts
@@ -2,5 +2,6 @@ export enum EventEmotionCollectEnum {
   like = 'like',
   start_playing = 'start_playing',
   skip = 'skip',
-  listen_through = 'listen_through'
+  listen_through = 'listen_through',
+  cron_collect = 'cron_collect'
 }
--- emotion-collector.service/src/enums/redis-schema.enum.ts
+++ emotion-collector.service/src/enums/redis-schema.enum.ts
@@ -1,5 +1,6 @@
 export enum RedisSchemaEnum {
   ListnerRegisterByEmail = 'ListnerRegisterByEmail',
   ContributorRegisterByEmail = 'ContributorRegisterByEmail',
-  StreamQueue = 'StreamQueue'
+  StreamQueue = 'StreamQueue',
+  emobeat_user_emotions = 'emobeat_user_emotions'
 }
